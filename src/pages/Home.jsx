d4f6a2696fe1cb1b3c4c87eb6c049b6973ef90f2
Davef <76170782+davefdev@users.noreply.github.com>
2022-07-18 17:58:34 +0100

    jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Container = styled.div`
+height: 60px;
+background-color: black;
+`
+
+
+const Home = () => {
+  return (
+    <Container>
+        <Navbar/>
+    </Container>
+  )
+}
+
+export default Home
