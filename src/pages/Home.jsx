18105bd889e189113068768a76074623c8841cff
Davef <76170782+davefdev@users.noreply.github.com>
2022-07-21 09:38:10 +0100

    jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Container = styled.div`
-height: 60px;
-background-color: black;
-`
+  height: 60px;
+  background-color: black;
+`;
 
 const Wrapper = styled.div`
+  padding: 10px 20px;
+`;
 
-padding: 10px 20px;
-`
+
 
 const Home = () => {
   return (
     <Container>
-      <Wrapper>
-        navbar
-              </Wrapper>
-
+      <Wrapper>navbar</Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
