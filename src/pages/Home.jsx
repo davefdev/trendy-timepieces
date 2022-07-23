a74f5e754959f9b5b6333b93a1d694b461d09419
Davef <76170782+davefdev@users.noreply.github.com>
2022-07-23 14:31:37 +0100

    jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import styled from "styled-components";
+import { Search } from "@material-ui/icons";
 
 const Container = styled.div`
   height: 60px;
@@ -8,6 +10,7 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
 `;
 
@@ -16,11 +19,30 @@ const Langauge = styled.span`
   cursor: pointer;
 `;
 
+const SearchContainer = styled.div`
+  border: 0.75px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
 `;
 const Center = styled.div`
   flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
 `;
 
 const Right = styled.div`
@@ -33,8 +55,14 @@ const Home = () => {
       <Wrapper>
         <Left>
           <Langauge>En</Langauge>
+          <SearchContainer>
+            <Input />
+            <Search />
+          </SearchContainer>
         </Left>
-        <Center>Center</Center>
+        <Center>
+          <Logo></Logo>
+        </Center>
         <Right>Right</Right>
       </Wrapper>
     </Container>
